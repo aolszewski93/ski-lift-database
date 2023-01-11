5ce635af3b63f631cf8794e56d0c78809990da3c
Albert Olszewski <al.m.olszewski@gmail.com>
2023-01-11 00:08:06 -0500

    step 2

--- webscraping_example.py
+++ webscraping_example.py
@@ -4,8 +4,22 @@ link: https://realpython.com/beautiful-soup-web-scraper-python/
 """
 
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)
+#prints the html to command line similar to inspect
+# print(page.text)
+
+#create a beautifulsoup object with the page content and choose the appropriate parser
+soup = BeautifulSoup(page.content, "html.parser")
+
+# grabs container for job postings
+results = soup.find(id="ResultsContainer")
+
+# create an iterable containing all of the html for all of the job postings. 
+job_elements = results.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    print(job_element, end="\n"*2)
