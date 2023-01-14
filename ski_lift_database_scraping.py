67c1bd9d7f181a3be62a52454915bd957240d0b3
Albert Olszewski <al.m.olszewski@gmail.com>
2023-01-14 17:44:59 -0500

    created script for generating the database

---
+++ ski_lift_database_scraping.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = "https://liftblog.com/united-states/"
+page = requests.get(URL)
+
+#create a beautifulsoup object with the page content and choose the appropriate parser
+soup = BeautifulSoup(page.content, "html.parser")
+
+# grabs container for job postings
+results = soup.find(id="post-1491")
+
+#there are two lists on this page.  first is the states and second is sharing links
+states = results.find_all("ul")[0].find_all("a")
+
+
+for state in states:
+    state_name = state.text.strip()
+    state_link = state['href']
+    print(state_name)
+    print(state_link)
